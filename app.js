587ba9c6201c10126394d6a9c2a5182fb92bd3e1
Glitch (node-web-console) <none>
2020-08-21 01:21:37 +0000

    Formatting.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express'),
-      path = require('path'),
-      exphbs  = require('express-handlebars'),
-      bodyParser = require('body-parser'),
+const express = require( 'express' ),
+      path = require( 'path' ),
+      exphbs  = require( 'express-handlebars' ),
+      bodyParser = require( 'body-parser' ),
       app = express();
 
-app.use(express.static('public'));
+app.use( express.static( 'public' ) );
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars');
+app.engine( 'handlebars', exphbs( {defaultLayout: 'main'} ) );
+app.set( 'views', __dirname + '/views' );
+app.set( 'view engine', 'handlebars' );
 
-app.use(bodyParser.urlencoded({
+app.use( bodyParser.urlencoded( {
   extended: true
-}));
+} ) );
 
 app.use( bodyParser.json() );
 
-app.use('/', require('./routes/index.js'))
+app.use( '/', require( './routes/index.js' ) )
 
 module.exports = app;
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -3,28 +3,28 @@
 module.exports = {
   getRandomFromArray: function( arr, n ) {
     n = n || 1;
-    let result = new Array(n),
+    let result = new Array( n ),
         len = arr.length,
-        taken = new Array(len);
-    if (n > len){
-      throw new RangeError("getRandom: more elements taken than available");
+        taken = new Array( len );
+    if ( n > len ){
+      throw new RangeError( "getRandom: more elements taken than available" );
     }
 
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
+    while ( n-- ) {
+        var x = Math.floor( Math.random( ) * len );
         result[n] = arr[x in taken ? taken[x] : x];
         taken[x] = --len in taken ? taken[len] : len;
     }
     return result;
   },
   getRandomInt: function( min, max ) {
-    return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+    return Math.floor( Math.random( ) * ( max - min + 1 ) ) + min;
   },
   getRandomRange: function( min, max, fixed ) {
-    return ( Math.random() * ( max - min ) + min ).toFixed( fixed ) * 1;
+    return ( Math.random( ) * ( max - min ) + min ).toFixed( fixed ) * 1;
   },
   numberWithCommas: function( x ){
-    return x.toString().replace( /\B( ?=( \d{3} )+( ?!\d ) )/g, ',' );
+    return x.toString( ).replace( /\B( ?=( \d{3} )+( ?!\d ) )/g, ',' );
   },
   isCommandline: function( userAgent ){
     check = false;
@@ -37,22 +37,22 @@ module.exports = {
   },
   getStream: function( req, res ){
     const { Readable } = require( 'stream' );
-    const stream = new Readable();
+    const stream = new Readable( );
 
     stream.pipe( res );
-    stream._read = function(){};
-    req.on( 'close', function(){
-      stream.destroy();
+    stream._read = function( ){};
+    req.on( 'close', function( ){
+      stream.destroy( );
     } );
-    req.on( 'error', function(){
-      stream.destroy();
+    req.on( 'error', function( ){
+      stream.destroy( );
     } );
     return stream;
   },
   symbols: {
     PAGE_BREAK: '\033[2J\033[H',
   },
-  getClientIp: function(req) {
+  getClientIp: function( req ) {
     var ipAddress;
     var forwardedIpsStr = req.header( 'x-forwarded-for' ); 
     if ( forwardedIpsStr ) {
@@ -64,5 +64,4 @@ module.exports = {
     }
     return ipAddress;
   }
-  
 };
