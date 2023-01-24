9e3b7ef62e5a3784ff486e43f6a683f5ed84097b
Glitch (node-web-console) <none>
2023-01-24 19:34:59 +0000

    Code cleanup.

--- helpers/hello.js
+++ helpers/hello.js
@@ -4,40 +4,40 @@
   See https://medium.com/@atulanand94/building-web-based-uis-for-terminals-using-javascript-60b5eee31213 for more details.
 */
 
-const style = require( 'ansi-styles' ),
-      helpers = require.main.require( './helpers/helpers.js' );
+const style = require("ansi-styles"),
+  helpers = require.main.require("./helpers/helpers.js");
 
-const delay = async function( milliseconds ) {
-  return new Promise( function( resolve ) {
-    setTimeout( function( ) {
-      return resolve( );
-    }, milliseconds );
-  } );
+const delay = async (milliseconds) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve();
+    }, milliseconds);
+  });
 };
 
-const hello = function( text ) {
+const hello = (text) => {
   /*
     You can use the ansi-styles package to change text color.
     See: https://www.npmjs.com/package/ansi-styles
   */
   return `
-${ helpers.symbols.PAGE_BREAK } 
-${style.green.open}Hi${ text }${style.green.close}
-\n`  
+${helpers.symbols.PAGE_BREAK} 
+${style.green.open}Hi${text}${style.green.close}
+\n`;
 };
 
-const animateHello = async function animateHello( stream ) {
-  for ( let i = 0; i < 20; i += 1 ) {
+const animateHello = async (stream) => {
+  for (let i = 0; i < 20; i += 1) {
     try {
-        if ( !stream.destroyed ){
-          stream.push( hello( `, it's ${new Date( )}` ) );
-        }      
-    } catch ( err ) {
+      if (!stream.destroyed) {
+        stream.push(hello(`, it's ${new Date()}`));
+      }
+    } catch (err) {
       /* noop */
     }
-    await delay( 1000 );
+    await delay(1000);
   }
-  stream.push( null );
+  stream.push(null);
 };
 
 module.exports = {
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,67 +1,69 @@
 /* Useful helper functions. */
 
 module.exports = {
-  getRandomFromArray: function( arr, n ) {
+  getRandomFromArray: (arr, n) => {
     n = n || 1;
-    let result = new Array( n ),
-        len = arr.length,
-        taken = new Array( len );
-    if ( n > len ){
-      throw new RangeError( "getRandom: more elements taken than available" );
+    let result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len) {
+      throw new RangeError("getRandom: more elements taken than available");
     }
 
-    while ( n-- ) {
-        var x = Math.floor( Math.random( ) * len );
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
     }
     return result;
   },
-  getRandomInt: function( min, max ) {
-    return Math.floor( Math.random( ) * ( max - min + 1 ) ) + min;
+  getRandomInt: (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-  getRandomRange: function( min, max, fixed ) {
-    return ( Math.random( ) * ( max - min ) + min ).toFixed( fixed ) * 1;
+  getRandomRange: (min, max, fixed) => {
+    return (Math.random() * (max - min) + min).toFixed(fixed) * 1;
   },
-  numberWithCommas: function( x ){
-    return x.toString( ).replace( /\B( ?=( \d{3} )+( ?!\d ) )/g, ',' );
+  numberWithCommas: (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   },
-  isCommandline: function( userAgent ){
+  isCommandline: (userAgent) => {
     check = false;
-    if ( userAgent ){
-      try{
-        check = userAgent.search( /curl|wget/i ) !== -1;
-      } catch( err ){ /* noop */ }
+    if (userAgent) {
+      try {
+        check = userAgent.search(/curl|wget/i) !== -1;
+      } catch (err) {
+        /* noop */
+      }
     }
     return check;
   },
-  getStream: function( req, res ){
-    const { Readable } = require( 'stream' );
-    const stream = new Readable( );
+  getStream: (req, res) => {
+    const { Readable } = require("stream");
+    const stream = new Readable();
 
-    stream.pipe( res );
-    stream._read = function( ){};
-    req.on( 'close', function( ){
-      stream.destroy( );
-    } );
-    req.on( 'error', function( ){
-      stream.destroy( );
-    } );
+    stream.pipe(res);
+    stream._read = () => {};
+    req.on("close", () => {
+      stream.destroy();
+    });
+    req.on("error", () => {
+      stream.destroy();
+    });
     return stream;
   },
   symbols: {
-    PAGE_BREAK: '\033[2J\033[H',
+    PAGE_BREAK: "\033[2J\033[H",
   },
-  getClientIp: function( req ) {
+  getClientIp: (req) => {
     var ipAddress;
-    var forwardedIpsStr = req.header( 'x-forwarded-for' ); 
-    if ( forwardedIpsStr ) {
-      var forwardedIps = forwardedIpsStr.split( ',' );
+    var forwardedIpsStr = req.header("x-forwarded-for");
+    if (forwardedIpsStr) {
+      var forwardedIps = forwardedIpsStr.split(",");
       ipAddress = forwardedIps[0];
     }
-    if ( !ipAddress ) {
+    if (!ipAddress) {
       ipAddress = req.connection.remoteAddress;
     }
     return ipAddress;
-  }
+  },
 };
